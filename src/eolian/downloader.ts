d93ab1b2f6b643fe2cc212e7d4a246df19d5b1a6
Jack Belford <jackbelford12@live.ca>
2021-07-04 15:12:53 -0700

    Reuse same function ref in downloader

--- src/eolian/downloader.ts
+++ src/eolian/downloader.ts
@@ -5,13 +5,14 @@ import { ContextMessage, ContextTextChannel } from './@types';
 export class DownloaderDisplay implements ProgressUpdater {
 
   private lastSent = 0;
-  private message: ContextMessage | null = null;
-  private updatePromise: Promise<void> | null = null;
+  private value = 0;
+  private message?: ContextMessage;
+  private updatePromise?: Promise<void>;
 
   constructor(private readonly channel: ContextTextChannel,
     private readonly name: string,
     private total: number = 1000,
-    private readonly refreshInterval = 1500) {
+    private readonly refreshInterval = 1000) {
   }
 
   init(total?: number): void {
@@ -23,24 +24,28 @@ export class DownloaderDisplay implements ProgressUpdater {
 
   update(value: number): void {
     if (this.updatePromise) {
-      this.updatePromise = this.updatePromise.then(async () => {
-        if (Date.now() - this.lastSent >= this.refreshInterval) {
-          this.lastSent = Date.now();
-          const text = `${this.name}: ${Math.round(100 * value / this.total)}%`;
-          if (this.message) {
-            await this.message.edit(text);
-          } else {
-            this.message = await this.channel.send(text) ?? null;
-          }
-        }
-      });
+      this.value = Math.round(100 * value / this.total);
+      this.updatePromise = this.updatePromise.then(this._update);
     }
   }
 
   async done(): Promise<void> {
     await this.updatePromise;
     await this.message?.edit(`${this.name}: 100%`);
-    this.updatePromise = null;
-    this.message = null;
+    this.updatePromise = undefined;
+    this.message = undefined;
   }
+
+  private _update = async () => {
+    if (Date.now() - this.lastSent >= this.refreshInterval) {
+      this.lastSent = Date.now();
+      const text = `${this.name}: ${this.value}%`;
+      if (this.message) {
+        await this.message.edit(text);
+      } else {
+        this.message = await this.channel.send(text);
+      }
+    }
+  }
+
 }
