599f7497777af8daccc88ba6f8c44edbea5ccd7e
Jack Belford <jackbelford12@live.ca>
2021-08-29 20:38:49 -0700

    Include total in servers command

--- src/commands/general/servers.ts
+++ src/commands/general/servers.ts
@@ -1,5 +1,6 @@
 import { Command, CommandContext, CommandOptions } from 'commands/@types';
 import { GENERAL_CATEGORY } from 'commands/category';
+import { PATTERNS } from 'commands/keywords';
 import { PERMISSION } from 'common/constants';
 
 const PAGE_LENGTH = 20;
@@ -8,18 +9,15 @@ async function execute(context: CommandContext, options: CommandOptions): Promis
   const servers = context.client.getServers();
 
   let start = 0;
-  if (options.ARG) {
-    const newPage = +options.ARG;
-    if (!isNaN(newPage) && newPage >= 0) {
-      start = newPage * PAGE_LENGTH;
-    }
+  if (options.NUMBER && options.NUMBER >= 0) {
+    start = options.NUMBER * PAGE_LENGTH;
   }
 
   if (start >= servers.length) {
     start = Math.max(0, servers.length - PAGE_LENGTH);
   }
 
-  let response = '```'
+  let response = `Total Servers: ${servers.length}\n` + '```'
   response += servers.slice(start, start + PAGE_LENGTH).map((server, i) => `${start + i + 1}. ${JSON.stringify(server)}`).join('\n');
   response += '\n```';
 
@@ -31,6 +29,7 @@ export const SERVERS_COMMAND: Command = {
   details: 'Show all servers this bot is joined to',
   permission: PERMISSION.OWNER,
   category: GENERAL_CATEGORY,
+  patterns: [PATTERNS.NUMBER],
   dmAllowed: true,
   usage: [
     {
